0642272c80caf1fb3c5ffdf9e73ce24cf5e02f0f
fox76055 <fox76055@gmail.com>
2026-07-05 12:12:05 +0500

    ship shields UI

--- Content.Client/Shuttles/UI/MapScreen.xaml.cs
+++ Content.Client/Shuttles/UI/MapScreen.xaml.cs
@@ -56,7 +56,6 @@ public sealed partial class MapScreen : BoxContainer
 
     private float _minMapDequeue = 0.05f;
     private float _maxMapDequeue = 0.25f;
-
     private StyleBoxFlat _ftlStyle;
 
     public event Action<MapCoordinates, Angle>? RequestFTL;
