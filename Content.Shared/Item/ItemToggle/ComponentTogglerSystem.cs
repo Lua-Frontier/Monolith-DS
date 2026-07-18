0b552cc3d00ee15aa9b6e5546929080052043405
Pcol <mandarincik3000@gmail.com>
2026-07-18 20:45:03 +0300

    Update ComponentTogglerSystem.cs

--- Content.Shared/Item/ItemToggle/ComponentTogglerSystem.cs
+++ Content.Shared/Item/ItemToggle/ComponentTogglerSystem.cs
@@ -1,12 +1,12 @@
 using Content.Shared.Item.ItemToggle.Components;
-using Robust.Shared.Timing;
+using Robust.Shared.Timing; // LuaM
 
 namespace Content.Shared.Item.ItemToggle;
 
 /// <summary>
 /// Handles <see cref="ComponentTogglerComponent"/> component manipulation.
 /// </summary>
-public sealed partial class ComponentTogglerSystem : EntitySystem
+public sealed partial class ComponentTogglerSystem : EntitySystem // LuaM: public sealed class > public sealed partial class
 {
     [Dependency] private IGameTiming _timing = default!; // LuaM: predict err fix
 
@@ -25,23 +25,27 @@ public sealed partial class ComponentTogglerSystem : EntitySystem
     // Goobstation - Make this system more flexible
     public void ToggleComponent(EntityUid uid, bool activate)
     {
-        if (!_timing.IsFirstTimePredicted) // LuaM: predict err fix
+        if (!_timing.IsFirstTimePredicted) // LuaM: !TryComp<ComponentTogglerComponent>(uid, out var component) > _timing.IsFirstTimePredicted
             return;
 
-        if (!TryComp<ComponentTogglerComponent>(uid, out var component))
+//         var target = component.Parent ? Transform(uid).ParentUid : uid; // Commented by LuaM
+        if (!TryComp<ComponentTogglerComponent>(uid, out var component)) // LuaM
             return;
 
-        // LuaM-start: add target for the correct remove component, in o.w. - wizden logic
         if (activate)
+// LuaM-start: add target for the correct remove component, in o.w. - wizden logic
         {
             var target = component.Parent ? Transform(uid).ParentUid : uid;
             if (TerminatingOrDeleted(target))
                 return;
 
             component.Target = target;
+// LuaM-end.
             EntityManager.AddComponents(target, component.Components);
         }
         else
+//             EntityManager.RemoveComponents(target, component.RemoveComponents ?? component.Components); // Commented by LuaM
+// LuaM-start:
         {
             if (component.Target == null)
                 return;
@@ -51,6 +55,6 @@ public sealed partial class ComponentTogglerSystem : EntitySystem
 
             EntityManager.RemoveComponents(component.Target.Value, component.RemoveComponents ?? component.Components);
         }
-        // LuaM-end
+ // LuaM-end
     }
 }
