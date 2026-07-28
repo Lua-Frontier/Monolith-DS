ac44b5f8e369c37830264ad5a26fb5e4e20ac54d
fox76055 <fox76055@gmail.com>
2026-07-28 20:08:31 +0500

    namespace change

--- Content.Goobstation.Shared/HoloCigar/HoloCigarSystem.cs
+++ Content.Goobstation.Shared/HoloCigar/HoloCigarSystem.cs
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Goobstation.Common.TheManWhoSoldTheWorld;
-using Content.Goobstation.Shared.Weapons.Multishot;
+using Content.Shared._Goobstation.Weapons.Multishot;
 using Content.Shared._Goobstation.Weapons.Ranged;
 using Content.Shared.Clothing.Components;
 using Content.Shared.Clothing.EntitySystems;
